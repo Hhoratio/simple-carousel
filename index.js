aa15d5d6fc9168fb43400c81b31e25e6998e1d5e
Horatio <silencerokstarr@gmail.com>
2021-12-21 12:13:13 +0800

    finalProect

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ let interval; // setInterval()
 // 將 interval 封裝出來
 function myInter() {
     interval = setInterval(() => {
+        // 當 當前圖片的索引位大於圖片長度 - 1 時，回到原點
         (index >=  minImgs.length - 1) ? index = 0: index++;
         ulMax.style.left = `${index * (-100)}%`;
     }, 3000)
 };
 
-// 網頁刷新先執行第一次
-ulMax.style.left = `${index * (-100)}%`;
-
 // 執行 interval
 myInter()
+
+// 圖片往左切換圖片按鈕
 leftBtn.addEventListener('click', () => {
     ( index == 0 || index == 1 ) ? index = 0: index = 1;
     ulMax.style.left = `${index * (-100)}%`;
@@ -26,10 +26,23 @@ leftBtn.addEventListener('click', () => {
     myInter();
 })
 
+// 圖片往右切換圖片按鈕
 rightBtn.addEventListener('click', () => {
     ( index == 1 || index == 2 ) ? index = 2: index++;
     ulMax.style.left = `${index * (-100)}%`;
     clearInterval(interval);
     myInter();
+})
+
+
+// 小圖標切換圖片
+minImgs.forEach( item => {
+    item.addEventListener('click', () => {
+        const imgIndex = [...minImgs].indexOf(item);
+        index = imgIndex;
+        ulMax.style.left = `${index * (-100)}%`;
+        clearInterval(interval);
+        myInter();
+    })
 })
 
